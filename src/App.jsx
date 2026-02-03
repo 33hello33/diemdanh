c3f9820e4d18b292d20ec80ee8e515a6394db54b
33hello33 <114247950+33hello33@users.noreply.github.com>
2026-02-03 12:28:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -472,7 +472,9 @@ async function loadThongKe() {
                 </option>
               ))}
             </select>
-
+            
+<input placeholder="Nội dung bài giảng hôm nay..." type="text" value="" style="width: 100%; margin-top: 6px; padding: 6px; border-radius: 6px; border: 1px solid rgb(204, 204, 204);">
+  
             <p>Tổng số học viên: {soLuongHocVien}</p>
 
             {students.map((s) => (
