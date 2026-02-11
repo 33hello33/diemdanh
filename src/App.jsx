42428ab169835c7dc862d27aa0cd9b31f7bfdcf0
33hello33 <114247950+33hello33@users.noreply.github.com>
2026-02-11 20:47:59 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import "./App.css";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function App() {
