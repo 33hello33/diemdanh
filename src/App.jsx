cece4a1e770441414744d69599982deaf2735006
33hello33 <114247950+33hello33@users.noreply.github.com>
2026-03-09 15:18:54 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ useEffect(() => {
     const { data: hv } = await supabase
       .from("tbl_hv")
       .select("*")
-      .eq("malop", maLop)
+      .or(`malop.eq.${maLop},malop.like.${maLop},malop.like.${maLop},%,malop.like.%,${maLop},%,malop.like.%,${maLop}`)
       .neq("trangthai", "Đã Nghỉ")
       .order("tenhv", { ascending: true });
 
